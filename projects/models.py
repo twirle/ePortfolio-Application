fb6eff5c1cf46730aea492b86ac26d695ef66986
twirle <s10179994@connect.np.edu.sg>
2019-11-18 15:40:30 +0800

    fixed images for projects, set up users, added projects

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=150)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    #image = models.ImageField(upload_to='img/')
+    image = models.FilePathField(path="img/")
